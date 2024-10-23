c9229af40e69fff758c572338d3c702022342890
thanhdicode <thanhhdse184021@fpt.edu.vn>
2024-10-23 11:26:28 +0700

    first commit

---
+++ store/app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard('1ed54d58-f16f-420e-8750-9c9cccdeedb3');
+    const products = await getProducts({ isFeatured: true })
+    return (
+        <Container>
+            <div className="pb-10 space-y-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default HomePage;
